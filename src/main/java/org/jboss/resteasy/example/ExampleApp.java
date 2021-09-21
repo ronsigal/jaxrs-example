0caeefc902b9f7f3318f0ea1464cec0ee28bbd6d
Ron Sigal <rsigal@redhat.com>
2021-09-21 15:59:14 -0400

    Adding classes, web.xml

---
+++ src/main/java/org/jboss/resteasy/example/ExampleApp.java
@@ -0,0 +1,9 @@
+package org.jboss.resteasy.example;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("root")
+public class ExampleApp extends Application {
+	
+} 
