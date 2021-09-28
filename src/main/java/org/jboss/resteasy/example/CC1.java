6ad634d5686e0151dc18d3d5d79b595b33e27e15
Ron Sigal <rsigal@redhat.com>
2021-09-28 19:50:06 -0400

    Changed GETs to POSTs

--- src/main/java/org/jboss/resteasy/example/CC1.java
+++ src/main/java/org/jboss/resteasy/example/CC1.java
@@ -1,6 +1,6 @@
 package org.jboss.resteasy.example;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 //import org.jboss.test.resteasy.CC1_Server;
@@ -11,7 +11,7 @@ public class CC1 {
 //   private CC1_Server server;
 
    @Path("m1")
-   @GET
+   @POST
    public String m1(CC2 cc2) {
       return "x";
    }
@@ -21,25 +21,25 @@ public class CC1 {
    }
 
    @Path("m3")
-   @GET
+   @POST
    public String m3(CC4 cc4) {
       return "x";
    }
 
    @Path("m4")
-   @GET
+   @POST
    public boolean m4(int i) {
       return true;
    }
 
    @Path("m5")
-   @GET
+   @POST
    public String m5() {
       return "m5";
    }
 
    @Path("m6")
-   @GET
+   @POST
    public CC4 m6(CC2 cc2) {
       CC5 cc5 = new CC5(cc2.j);
       System.out.println("cc2.s: " + cc2.s + ", cc5.k: " + cc5.k);
@@ -47,37 +47,37 @@ public class CC1 {
    }
 
    @Path("short")
-   @GET
+   @POST
    public short getShort(short n) {
 	   return short.class.cast(n + 1);
    }
 
    @Path("Short")
-   @GET
+   @POST
    public Short getShortWrapper(Short n) {
 	   return Short.valueOf((short) (n.shortValue() + 1));
    }
 
    @Path("int")
-   @GET
+   @POST
    public int getInt(int n) {
 	   return n + 1;
    }
 
    @Path("Integer")
-   @GET
+   @POST
    public Integer getInteger(Integer n) {
 	   return Integer.valueOf(n.intValue() + 1);
    }
 
    @Path("long")
-   @GET
+   @POST
    public long getLong(long n) {
 	   return n + 1;
    }
 
    @Path("Long")
-   @GET
+   @POST
    public Long getLongWrapper(Long n) {
 	   return Long.valueOf(n.longValue() + 1);
    }
