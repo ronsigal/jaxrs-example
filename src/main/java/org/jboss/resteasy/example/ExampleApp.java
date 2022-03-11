6a30c20ca018c10a339c4e5af3b94b7146965ef1
Ron Sigal <rsigal@redhat.com>
2022-03-11 15:59:48 -0500

    Some renaming

--- src/main/java/org/jboss/resteasy/example/ExampleApp.java
+++ src/main/java/org/jboss/resteasy/example/ExampleApp.java
@@ -1,9 +1,17 @@
 package org.jboss.resteasy.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("root")
 public class ExampleApp extends Application {
-	
+   
+   public Set<Class<?>> getClasses() {
+      Set<Class<?>> set = new HashSet<Class<?>>();
+      set.add(CC1.class);
+      return set;
+   }
 } 
